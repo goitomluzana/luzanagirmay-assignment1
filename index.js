6f49702f157ce109812fc83b3f2937a24b946ea9
Luzana Girmay <luzz.anna12@gmail.com>
2019-04-15 17:33:15 -0500

    Commit more files

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Where the magic happens!');
+console.log(document);
+console.log($(document))
+function red() {
+    //change the page's background color to red
+    document.body.style.backgroundColor = "red";
+}
+
+function blue(){
+    //change the page's background color sto white
+    document.body.style.backgroundColor = "blue";
+} 
